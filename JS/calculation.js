f141300f287bb8dc3894686dbd6d7cd268691229
jewelCode <jewelbarua101@gmail.com>
2022-02-16 22:30:27 +0600

    income expense calculator project started

---
+++ JS/calculation.js
@@ -0,0 +1,29 @@
+document.getElementById("calculation").addEventListener("click", function(){
+    const inputIncome = document.getElementById("input-income").value;
+
+    const totalIncome = parseInt(inputIncome);
+
+
+    // Food Expenses
+    const inputFoodExpenses = document.getElementById("input-food-expenses").value;
+    const foodExpenses = parseInt(inputFoodExpenses);
+
+    // Rent Expenses
+
+    const inputRentExpenses = document.getElementById("input-rent-expenses").value;
+    const rentExpenses = parseInt(inputRentExpenses);
+  
+     // Clothes Expenses
+    const inputClothesExpenses = document.getElementById("input-clothes-expenses").value;
+    const clothesExpenses = parseInt(inputClothesExpenses);
+   
+    
+    const totalExpenses = foodExpenses + rentExpenses + clothesExpenses;
+    // console.log(totalExpenses);
+    
+
+    // Total Balance Amount
+    const balance = totalIncome - totalExpenses;
+    console.log(balance);
+
+})
